46992db91182af2cb36b022aee51574b4bcbdbb3
IldikoPilan <ildiko.pilan@gmail.com>
2020-04-28 14:12:33 +0200

    R minor updates

--- plot_pedigree.r
+++ plot_pedigree.r
@@ -3,18 +3,17 @@
 library(kinship2)
 
 plot_pedigree <- function(args) {
-	p1 <- read.table(args[1], header=T) 	# indexing starts with 1				
-	p2 <- as.data.frame(p1)				 	# transform to dataframe object
-	attach(p2) 			 					# attach R object to search path				
-	ped <- pedigree(id,fid,mid,sex,aff) 	# convert to pedigree obj	
-	par(xpd = TRUE)	 						# set graph params
-	png(file=args[2])					 	# get file name
-	names <- strsplit(args[3], ",")[[1]] 	# need to index first element to get list
-	id2 <- paste(ped$id, names, sep = "\n") # add family member names to IDs
-	plot(ped,id=id2) 						# create plot
+	pd <- read.table(args[1], header=T) 		# indexing starts with 1				
+	pd_df <- as.data.frame(pd)					# transform to dataframe object
+	attach(pd_df) 			 					# attach R object to search path				
+	ped <- pedigree(id,fid,mid,sex,aff) 		# convert to pedigree obj	
+	par(xpd = TRUE)	 							# set graph params
+	png(file=args[2])					 		# get file name
+	names <- strsplit(args[3], ",")[[1]] 		# need to index first element to get list
+	id2names <- paste(ped$id, names, sep = "\n")# add family member names to IDs
+	plot(ped,id=id2names) 						# create plot
 }
 
 # Parse arguments
 args = commandArgs(trailingOnly=TRUE)
-
 plot_pedigree(args)
